ae04b4884480a5cdb37f2ccaebe33b652d39a346
Shouvik Chaudhury <shouvik.chaudhury@kellogg.northwestern.edu>
2024-02-16 00:59:54 +0000

    commit 1

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,29 @@
+class PlacesController < ApplicationController
+
+  def index
+    # find all places visited
+    @places = Place.all
+    # render places/index view
+  end  
+
+  def show
+    # find a place
+    @place = Place.find_by({"id" => params["id"]})
+  end  
+
+  def new
+    # render view with new place form
+  end  
+
+  def create
+    # start with a new place
+    @place = Place.new
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["name"]
+    # save Place row
+    @place.save
+    # redirect user
+    redirect_to "/places"
+  end
+
+end
